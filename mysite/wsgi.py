2d965e14e5050c5be0b97a0b669f912eb5ab84e2
Gabriela Stamatov <youremail@whatever.com>
2026-04-23 19:56:28 +0100

    test

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -4,21 +4,13 @@ WSGI config for mysite project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-=======
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
->>>>>>> 43179ae0b1d187050f65b771d77fecad00021062
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-<<<<<<< HEAD
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-=======
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
->>>>>>> 43179ae0b1d187050f65b771d77fecad00021062
 
 application = get_wsgi_application()
